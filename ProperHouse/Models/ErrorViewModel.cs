9b3225f18160ebb6b6037a6e56fcf09c6a8fc7bd
rende <rende@LAPTOP-9G626E23>
2022-03-14 20:18:04 +0200

    Add project files.

---
+++ ProperHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProperHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
